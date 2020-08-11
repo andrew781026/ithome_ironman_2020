c969f87c91f446cc89596a81f560d030cb3a4212
王澍(Andrew) <andrew7810262000@gmail.com>
2020-08-11 17:24:11 +0800

    [day-02] 預設快捷鍵說明

--- day-02/main.js
+++ day-02/main.js
@@ -1,7 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
-require('electron-reload')(__dirname);
+// require('electron-reload')(__dirname);
 
 function createWindow() {
     // Create the browser window.
