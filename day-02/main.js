8d4e9db9e965ba0e44b61a5db3af629d1271ea04
王澍(Andrew) <andrew7810262000@gmail.com>
2020-08-20 17:49:43 +0800

    [day-02] add cat gif

--- day-02/main.js
+++ day-02/main.js
@@ -1,144 +1,76 @@
 // Modules to control application life and create native browser window
-const {app, Menu, isMac, BrowserWindow} = require('electron');
+const {app, Menu, BrowserWindow} = require('electron');
 const path = require('path');
 // require('electron-reload')(__dirname);
-
-// 使用 vue.js 開發 , 然後將畫面改到 electron 上面 (將編譯後的 vue 轉到 main.js 上面)
-
-const template = [
-    // { role: 'appMenu' }
-    ...(isMac ? [{
-        label: app.name,
-        submenu: [
-            {role: 'about'},
-            {type: 'separator'},
-            {role: 'services'},
-            {type: 'separator'},
-            {role: 'hide'},
-            {role: 'hideothers'},
-            {role: 'unhide'},
-            {type: 'separator'},
-            {role: 'quit'}
-        ]
-    }] : []),
-    // { role: 'fileMenu' }
-    {
-        label: '檔案',
-        submenu: [
-            isMac ? {label: '離開', role: 'close'} : {label: '離開', role: 'quit'}
-        ]
-    },
-    // { role: 'editMenu' }
-    {
-        label: 'Edit',
-        submenu: [
-            {role: 'undo'},
-            {role: 'redo'},
-            {type: 'separator'},
-            {role: 'cut'},
-            {role: 'copy'},
-            {role: 'paste'},
-            {role: 'delete'},
-            {type: 'separator'},
-            {
-                label: '開發者工具',
-                role: 'toggledevtools'
-            }
-        ]
-    },
-    // { role: 'viewMenu' }
-    {
-        label: '查看',
-        submenu: [
-            {role: 'reload'},
-            {role: 'forcereload'},
-            {
-                label: '開發者工具',
-                role: 'toggledevtools'
-            },
-            {type: 'separator'},
-            {role: 'resetzoom'},
-            {role: 'zoomin'},
-            {role: 'zoomout'},
-            {type: 'separator'},
-            {role: 'togglefullscreen'}
-        ]
-    },
-    // { role: 'windowMenu' }
-    {
-        label: 'Window',
-        submenu: [
-            {role: 'minimize'},
-            {role: 'zoom'},
-            ...(isMac ? [
-                {type: 'separator'},
-                {role: 'front'},
-                {type: 'separator'},
-                {role: 'window'}
-            ] : [
-                {role: 'close'}
-            ])
-        ]
-    },
-    {
-        role: 'help',
-        submenu: [
-            {
-                label: 'Learn More',
-                click: async () => {
-                    const {shell} = require('electron');
-                    await shell.openExternal('https://electronjs.org')
-                }
-            }
-        ]
-    }
-];
 
 function createWindow() {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 350,
+        height: 350,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             // nodeIntegration: false,
         },
-        // frame: false,
-        // autoHideMenuBar: true
+        frame: false,      // 標題列不顯示
+        transparent: true, // 背景透明
+        autoHideMenuBar: true //  工具列不顯示
     });
+
+    const template = [
+        {label: '關閉', role: 'close'},
+        {label: '全螢幕', role: 'togglefullscreen'},
+        {label: '刷新', role: 'reload'},
+    ];
 
     const menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu);
-    // mainWindow.setMenu(menu);
 
-
-    // and load the index.html of the app.
-    // mainWindow.loadFile('index.html')
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    // mainWindow.loadURL(`https://www.google.com`);
+    mainWindow.loadFile('index.html');
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+// Windows 工作列 - https://www.electronjs.org/docs/tutorial/windows-taskbar
+app.setJumpList([
+    {
+        name: '最近訪問',
+        items: [
+            {
+                type: 'task',
+                title: '開啟 Chrome',
+                program: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+                icon: 'C:\\Users\\andrew\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1\\Google Profile.ico',
+                description: '開新 Chrome 瀏覽器'
+            },
+        ]
+    },
+    { // has no name and no type so `type` is assumed to be "tasks"
+        items: [
+            {
+                type: 'task',
+                title: 'New Project',
+                program: process.execPath,
+                args: '--new-project',
+                description: 'Create a new project.'
+            },
+            { type: 'separator' },
+            {
+                type: 'task',
+                title: 'Recover Project',
+                program: process.execPath,
+                args: '--recover-project',
+                description: 'Recover Project'
+            }
+        ]
+    }
+])
+
+app.on('ready', () => {
+
     createWindow();
+})
 
-    app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-});
+app.on('window-all-closed', () => {
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') app.quit()
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+    app.quit();
+})
--- day-02/readme.md
+++ day-02/readme.md
@@ -1,8 +1,22 @@
 # 第二天 - 股票查價(一) - 用 electron-quick-start 當初始版型
 
+今天先動手做一個 Electron 小程式 , 感受一下 Electron 
+
+首先 , 我們先做個療癒小貓咪放到桌面上吧
+
+
+預計效果
+![](https://i.imgur.com/MfEjj5I.gif)
+
+小貓圖 - 可愛吧 😁
+
+![](https://i.imgur.com/6O1RzBu.gif)
+
 `npm init` 建立 package.json 檔案
 
 `npm i -D electron electron-reload` 將 electron 套件
+
+[frameless-window](https://www.electronjs.org/docs/api/frameless-window)
 
 ## 快捷鍵
 
@@ -27,8 +41,10 @@
 3. 設定到 BrowserWindow 中
 
 ## 參考資料 
+- [emoji list](https://getemoji.com/)
 - [electron-quick-start](https://github.com/electron/electron-quick-start)
 - [顏文字](https://honeygal.pixnet.net/blog/post/32779199-%E8%BD%89%E9%8C%84::%E8%A1%A8%E6%83%85%E7%AC%A6%E8%99%9F-%E9%A1%8F%E6%96%87%E5%AD%97)
 - [electron reload shortcut](https://github.com/electron/electron/issues/11895)
 - [Electron - Debugging](https://www.tutorialspoint.com/electron/electron_debugging.htm)
 - [electron 自定義快捷鍵](https://www.electronjs.org/docs/tutorial/keyboard-shortcuts)
+- [canvas_clock](https://www.w3schools.com/graphics/canvas_clock_start.asp)
