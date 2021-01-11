04768bd284ccbc937accaaf071fa58e4311991e6
王澍(Andrew) <andrew7810262000@gmail.com>
2021-01-11 11:28:35 +0800

    刪除未用到的檔案

--- --day-47/preload.js
+++
@@ -1,9 +0,0 @@
-const {ipcRenderer} = require('electron');
-
-window.addEventListener('DOMContentLoaded', () => {
-
-    const img = document.getElementById('img');
-    const switchCat = number => img.src = `imgs/cat_0${number}.gif`;
-
-    ipcRenderer.on('switch-cat', (event, args) => switchCat(args));
-});
